48578c58cbe39df7d589f20653e68e191974dc1f
Mert Buldur <buldurmert@gmail.com>
2021-10-03 14:18:02 +0300

    Fix

--- index.php
+++ index.php
@@ -1,18 +1,18 @@
 <?php
-require_once 'src/MTBackup.php';
+require_once 'src/BackupService.php';
 
 $BackupService = new \MTBackup\BackupService([
     'host'=>'localhost',
     'database'=>'laravel',
     'user'=>'root',
-    'password'=>'123123asdasd'
+    'password'=>''
 ],[
     'smtp'=>'smtp.mailgun.org',
     'port'=>587,
     'secure'=>'tls',
-    'username'=>'postmaster@sandbox65b054327acb4e4aa98c22e3ad75528e.mailgun.org',
-    'password'=>'5b3d1bdebbbd40c5c7caa8679f329c8a-dbdfb8ff-a2acb932',
-    'backup_mail'=>'buldurmert@gmail.com'
+    'username'=>'',
+    'password'=>'',
+    'backup_mail'=>''
 ]);
 //
 //$BackupService->backup()->write()->mail();
